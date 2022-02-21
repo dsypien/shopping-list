483a0c0008dd096120e2f3af040f774d46656bc3
David Sypien <davidsypien@gmail.com>
2022-02-21 06:35:27 -0500

    provide App with access to redux store

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import shoppingListReducer from './reducers/shoppingList';
+import logger from 'redux-logger';
+
+const store = createStore(
+  shoppingListReducer,
+  applyMiddleware(logger)
+)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
