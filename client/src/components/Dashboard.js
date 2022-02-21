2ef338039dc4fa940b6754c2dae8dadc50a69fdd
David Sypien <davidsypien@gmail.com>
2022-02-21 08:26:54 -0500

    render emptyshoppinglist component if no items in shoppinglist

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,15 +1,35 @@
 import React from "react";
 import ShoppingList from "./ShoppingList";
+import EmptyShoppingList from "./EmptyShoppingList";
 import AddItem from "./AddItem";
+import { connect } from "react-redux";
 
-const Dashboard = () => {
+const Dashboard = (props) => {
+   const { itemCount } = props;
+
+   const renderDashboard = () => {      
+      if(itemCount > 0){
+         return <ShoppingList />
+      } else {
+         return <EmptyShoppingList />
+      }
+   }
+
    return (
-      <>
+      <div className="dashboard">
          Dashboard
-         <ShoppingList />
+         {renderDashboard()}
          <AddItem />
-      </>
+      </div>
    )
 }
 
-export default Dashboard;
+function mapStateToProps ({shoppingList}) {
+   const itemCount = Object.keys(shoppingList).length;
+
+   return {
+      itemCount,
+   }
+}
+
+export default connect()(Dashboard);
--- client/src/components/EmptyShoppingList.js
+++ client/src/components/EmptyShoppingList.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import { Button } from "@mui/material";
 
 class EmptyShoppingList extends Component {
    render () {
       return (
          <>
-            Empty Shopping List
+            <h3>Your shopping list is empty :(</h3>)
+            <Button 
+               size="large"
+               variant="contained"
+               onClick={this.onAddItem}>
+               Add your first Item
+            </Button>
          </>
       )
    }
