a5f3fdb4cdb697a845da097651292f9f6081dbb1
David Sypien <davidsypien@gmail.com>
2022-02-21 10:37:37 -0500

    Move Shopping List item markup to shopping list item from shopping list

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -2,9 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import { Checkbox } from "@mui/material";
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
+import ShoppingListItem from "./ShoppingListItem";
 
 const ShoppingList = (props) => {   
    const {list} = props;
@@ -13,6 +11,8 @@ const ShoppingList = (props) => {
    const onAddItem = () => {
       navigate("/add");
    }
+
+
 
    return (
       <>
@@ -28,23 +28,7 @@ const ShoppingList = (props) => {
          </div>
          <div className="shopping-list">               
             {list && list.map(item => (
-               <div className="shopping-list-item mb-3">
-                  <div className="flex-container" key={item.id}>
-                     <div className="left-container"> 
-                        <Checkbox style={{padding: "12px"}} />
-                     </div>
-                     <div className="shopping-list-body">
-                        <div>
-                           <h3>{item.itemName} ({item.quantity})</h3>
-                        </div>
-                        <span>{item.description}</span>                        
-                     </div>
-                     <div className="right-container">
-                        <EditIcon />
-                        <DeleteIcon className="ml-1"/>
-                     </div>
-                  </div>
-               </div>
+               <ShoppingListItem item={item} />
             ))}
          </div>         
       </>
--- client/src/components/ShoppingListItem.js
+++ client/src/components/ShoppingListItem.js
@@ -1,13 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
+import { Checkbox } from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-class ShoppingListItem extends Component {
-   render () {
-      return (
-         <>
-            Shopping List Item
-         </>
-      )
-   }
+const ShoppingListItem = (props) => {
+   const { item } = props;
+
+   return (
+      <div className="shopping-list-item mb-3">
+         <div className="flex-container" key={item.id}>
+            <div className="left-container"> 
+               <Checkbox style={{padding: "12px"}} />
+            </div>
+            <div className="shopping-list-body">
+               <div>
+                  <h3>{item.itemName} ({item.quantity})</h3>
+               </div>
+               <span>{item.description}</span>                        
+            </div>
+            <div className="right-container">
+               <EditIcon />
+               <DeleteIcon className="ml-1"/>
+            </div>
+         </div>
+      </div>
+   )
 }
 
 export default ShoppingListItem;
