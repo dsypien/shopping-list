93fc3165c62bcaae935c11430321055d13de4103
David Sypien <davidsypien@gmail.com>
2022-02-21 18:50:06 -0500

    Add delete item dialog

---
+++ client/src/components/DeleteItemDialog.js
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { connect } from 'react-redux';
+import { deleteShoppingListItem } from '../actions/shoppingList';
+
+const DeleteItemDialog = (props) => {
+   const [open, setOpen] = React.useState(false);
+   const {id} = props;
+
+   const handleClickOpen = () => {
+      setOpen(true);
+   };
+
+   const handleClose = () => {
+      setOpen(false);
+   };
+
+   const handleDelete = () => {
+      props.dispatch(deleteShoppingListItem(id));
+      setOpen(false);
+   }
+
+   return (
+      <div>
+         <DeleteIcon onClick={handleClickOpen} />
+         <Dialog
+         open={open}
+         onClose={handleClose}
+         aria-labelledby="alert-dialog-title"
+         aria-describedby="alert-dialog-description"
+         >
+         <DialogTitle id="alert-dialog-title">
+            {"Delete Item?"}
+         </DialogTitle>
+         <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+            Are you sure you want to delete this item?  This cannot be undone.
+            </DialogContentText>
+         </DialogContent>
+         <DialogActions>
+            <Button
+               size="large"
+               variant="outlined" 
+               onClick={handleClose}>
+                  Cancel
+            </Button>
+            <Button 
+               size="large"
+               variant="contained"
+               onClick={handleDelete} autoFocus>
+               Delete
+            </Button>
+         </DialogActions>
+         </Dialog>
+      </div>
+   );
+}
+
+export default connect()(DeleteItemDialog)
