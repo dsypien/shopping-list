defc9fc5eadc450372c14693504644d9a774fa22
David Sypien <davidsypien@gmail.com>
2022-02-21 07:45:17 -0500

    Add Item component has all its fields, connected to store

--- client/src/components/AddItem.js
+++ client/src/components/AddItem.js
@@ -1,13 +1,89 @@
 import React, { Component } from "react";
+import { TextField } from "@mui/material";
+import Dropdown from "./InputFields/Dropdown";
+import { Button } from "@mui/material";
+import { connect } from "react-redux";
+import { createShoppingListItem } from "../actions/shoppingList";
 
 class AddItem extends Component {
+   state = {
+      id: this.props.id || null,
+      itemName: this.props.itemName || "",
+      description: this.props.description || "",
+      quantity: this.props.quantity || "",
+      didPurchase: this.props.didPurchase || false
+   }
+
+   quantityOptions = [{value: 1, text: 1}, {value: 2, text: 2}, {value: 3, text: 3}]
+
+   setInputValue = (e) => {
+      if (e.target.type === "checkbox") {
+         this.setState({
+            [e.target.name] : e.target.checked
+         })
+      } else {
+         this.setState({
+            [e.target.name]: e.target.value
+         })
+      }
+   }
+
+   onAddItem = () => {
+      this.props.dispatch(createShoppingListItem(this.state));
+   }
+
+   onCancel = () => {
+      // TODO: navigate back to dashboard  
+   }
+
    render () {
       return (
-         <>
-            Add Item
-         </>
+         <div className="item-form">
+            <h2>Add an Item</h2>
+            <TextField
+               style={{width:"100%", margin: "10px"}}
+               name="itemName"
+               label="Item Name"
+               value={this.state.itemName}
+               onChange={(e) => this.setInputValue(e) }
+               variant="outlined"
+               autoComplete="off" />
+            <TextField
+               style={{width:"100%", margin: "10px"}}
+               name="description"
+               label="Description"
+               multiline={true}
+               rows={5}
+               value={this.state.description}
+               onChange={(e) => this.setInputValue(e) }
+               variant="outlined"
+               autoComplete="off" />
+            <Dropdown
+               name="quantity"
+               label="How many?"
+               value={this.state.quantity}
+               options={this.quantityOptions}
+               handleChange={(e) => this.setInputValue(e)}
+            />
+            <div style={{display: "flex", justifyContent: "space-evenly", margin: "25px"}}>
+               <Button 
+                  size="large"
+                  variant="outlined"
+                  onClick={this.onCancel}>
+                  Cancel
+               </Button>
+               <Button 
+                  size="large"
+                  variant="contained"
+                  onClick={this.onAddItem}>
+                  Add Item
+               </Button>
+               
+            </div>
+            
+         </div>
       )
    }
 }
 
-export default AddItem;
+export default connect()(AddItem);
