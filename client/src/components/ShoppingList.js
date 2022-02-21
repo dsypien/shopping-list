a8492bfcc1b9cbfe8bd0e9f2028d8ee8f783e44d
David Sypien <davidsypien@gmail.com>
2022-02-21 10:32:23 -0500

    Add some styling to shopping list

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,24 +1,54 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { Checkbox } from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-class ShoppingList extends Component {   
-   render () {
-      const {list} = this.props;
-      return (
-         <>
-            <h2>Shopping List</h2>
-            <table>               
-               {list && list.map(item => (
-                  <tr key={item.id}>
-                     <td>{item.itemName}</td>
-                     <td>{item.description}</td>
-                     <td>{item.quantity}</td>
-                  </tr>
-               ))}
-            </table>
-         </>
-      )
+const ShoppingList = (props) => {   
+   const {list} = props;
+   const navigate = useNavigate();
+
+   const onAddItem = () => {
+      navigate("/add");
    }
+
+   return (
+      <>
+         <h2>Shopping List</h2>
+         <div style={{display: "flex", justifyContent: "space-between", margin: "25px"}}>
+            <h3>Your items</h3>
+            <Button 
+               size="small"
+               variant="contained"
+               onClick={onAddItem}>
+               Add Item
+            </Button>               
+         </div>
+         <div className="shopping-list">               
+            {list && list.map(item => (
+               <div className="shopping-list-item mb-3">
+                  <div className="flex-container" key={item.id}>
+                     <div className="left-container"> 
+                        <Checkbox style={{padding: "12px"}} />
+                     </div>
+                     <div className="shopping-list-body">
+                        <div>
+                           <h3>{item.itemName} ({item.quantity})</h3>
+                        </div>
+                        <span>{item.description}</span>                        
+                     </div>
+                     <div className="right-container">
+                        <EditIcon />
+                        <DeleteIcon className="ml-1"/>
+                     </div>
+                  </div>
+               </div>
+            ))}
+         </div>         
+      </>
+   )
 }
 
 function mapStateToProps ({shoppingList}) {
